591068bc839dc7462d91e2e205502c1e30737b3a
pritomguha62 <pritomguha62@gmail.com>
2024-04-13 14:01:23 +0600

    Admin Panel Update.

--- resources/views/admin_view/common/all_admins.blade.php
+++ resources/views/admin_view/common/all_admins.blade.php
@@ -49,50 +49,52 @@
                         $sl = 1;
                     @endphp
                     @foreach ($all_admins as $all_admin)
-                        <form action="{{ route('update_admin') }}" method="post">
-                            <tr>
-                                @csrf
-                                <td>{{ $sl }}</td>
-                                {{-- <td>{{ $all_admin->serial_no }}</td> --}}
-                                <td>{{ $all_admin->name }}</td>
-                                <td><a href="mailto:{{ $all_admin->email }}">{{ $all_admin->email }}</a></td>
-                                <td><a href="tel:{{ $all_admin->phone }}">{{ $all_admin->phone }}</a></td>
-                                <td>{{ $all_admin->birth_date }}</td>
-                                <td>
-                                    <select name="status" id="" class="form-control">
-                                        <option selected value="{{ $all_admin->status }}">
-                                            @if ($all_admin->status == 1)
-                                                Active
-                                            @else
-                                                Deactive
-                                            @endif
-                                        </option>
-                                        <option value="1">Activate</option>
-                                        <option value="0">Deactivate</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    <select name="role_id" id="" class="form-control">
-                                        <option selected value="{{ $all_admin->role_id }}">
-                                            @if ($all_admin->role_id == 1)
-                                                Admin
-                                            @else
-                                                Teacher
-                                            @endif
-                                        </option>
-                                        <option value="1">Admin</option>
-                                        <option value="2">Teacher</option>
-                                    </select>
-                                </td>
-                                <td>
-                                    <input type="hidden" name="admin_id" hidden value="{{ $all_admin->admin_id }}">
+                        @if ($all_admin->email != 'pritomguha62@gmail.com')
+                            <form action="{{ route('update_admin') }}" method="post">
+                                <tr>
+                                    @csrf
+                                    <td>{{ $sl }}</td>
+                                    {{-- <td>{{ $all_admin->serial_no }}</td> --}}
+                                    <td>{{ $all_admin->name }}</td>
+                                    <td><a href="mailto:{{ $all_admin->email }}">{{ $all_admin->email }}</a></td>
+                                    <td><a href="tel:{{ $all_admin->phone }}">{{ $all_admin->phone }}</a></td>
+                                    <td>{{ $all_admin->birth_date }}</td>
+                                    <td>
+                                        <select name="status" id="" class="form-control">
+                                            <option selected value="{{ $all_admin->status }}">
+                                                @if ($all_admin->status == 1)
+                                                    Active
+                                                @else
+                                                    Deactive
+                                                @endif
+                                            </option>
+                                            <option value="1">Activate</option>
+                                            <option value="0">Deactivate</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <select name="role_id" id="" class="form-control">
+                                            <option selected value="{{ $all_admin->role_id }}">
+                                                @if ($all_admin->role_id == 1)
+                                                    Admin
+                                                @else
+                                                    Teacher
+                                                @endif
+                                            </option>
+                                            <option value="1">Admin</option>
+                                            <option value="2">Teacher</option>
+                                        </select>
+                                    </td>
+                                    <td>
+                                        <input type="hidden" name="admin_id" hidden value="{{ $all_admin->admin_id }}">
 
-                                    <input type="submit" class="btn btn-sm btn-success" value="Upadate">
-                                    
-                                    <a href="{{ route('delete_admin', ['admin_id'=>$all_admin->admin_id]) }}" class="btn btn-sm btn-danger">Delete</a>
-                                </td>
-                            </tr>
-                        </form>
+                                        <input type="submit" class="btn btn-sm btn-success" value="Upadate">
+                                        
+                                        <a href="{{ route('delete_admin', ['admin_id'=>$all_admin->admin_id]) }}" class="btn btn-sm btn-danger">Delete</a>
+                                    </td>
+                                </tr>
+                            </form>
+                        @endif
                         @php
                             $sl++;
                         @endphp
