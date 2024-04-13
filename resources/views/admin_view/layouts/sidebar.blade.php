e995580a3acc4929b7dbbed952781e4e36af1813
pritomguha62 <pritomguha62@gmail.com>
2024-04-13 13:47:21 +0600

    Admin Panel Logo Update.

--- resources/views/admin_view/layouts/sidebar.blade.php
+++ resources/views/admin_view/layouts/sidebar.blade.php
@@ -5,7 +5,8 @@
             <!-- Sidebar - Brand -->
             <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('dashboard') }}">
                 <div class="sidebar-brand-icon rotate-n-15">
-                    <i class="fas fa-laugh-wink"></i>
+                    {{-- <i class="fas fa-laugh-wink"></i> --}}
+                    <img src="{{ asset('pv_assets/images/logo.png') }}" alt="MediaTTC">
                 </div>
                 <div class="sidebar-brand-text mx-3">MediaTTC</div>
             </a>
